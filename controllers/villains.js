e4fa89d3d7ea4c7b025d68cfe295abab2d8edb58
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-02-26 16:53:20 -0500

    getting error on .calledWith. 2 tests passing 3 failing

--- controllers/villains.js
+++ controllers/villains.js
@@ -1,24 +1,36 @@
 const models = require('../models')
 
 const getAllVillains = async (request, response) => {
+try{
   const villains = await models.villains.findAll()
 
   return response.send(villains)
+} catch(error) {
+  return response.status(500).send('Unable to retreive villain. Please try again.')
+}
 }
 const getVillainBySlug = async (request, response) => {
+  try{
   const { slug } = request.params
   const matchedVillains = await models.villains.findOne({ where: { slug } })
 
   return matchedVillains
     ? response.send(matchedVillains)
     : response.sendStatus(404)
+  } catch(error) {
+    return response.status(500).send('Unable to retreive villain. Please try again.')
+  }
 }
 
 const saveNewVillain = async (request, response) => {
+  try{
   const { name, movie, slug } = request.body
 
   if (!name || !movie || !slug) {
     return response.status(400).send('The following paraneters are required: name, movie, slug')
+  }
+} catch(error) {
+    return response.status(500).send('Unable to create villain. Please try again.')
   }
 
   const newVillain = await models.villains.create({ name, movie, slug })
--- tests/controllers/villains.test.js
+++ tests/controllers/villains.test.js
@@ -1,5 +1,5 @@
-const { after, afterEach, before, describe, it } = require('mocha')
 const chai = require('chai')
+const { after, afterEach, before, beforeEach, describe, it } = require('mocha')
 const { createSandbox } = require('sinon')
 const sinonChai = require('sinon-chai')
 const models = require('../../models')
@@ -15,26 +15,38 @@ const {getAllVillains,
     const { expect } = chai
 
     describe('Controllers - Villains', (req, resp) => {
-        
-            let sandbox 
             let response
+            let sandbox 
+            let stubbedFindAll
+            let stubbedFindOne
             let stubbedSend
             let stubbedSendStatus
-            let stubbedFindAll
+            let stubbedStatus
+            let stubbedStatusDotSend
+
 
             before(() => {
                 sandbox = createSandbox()
+                
+                stubbedFindAll = sandbox.stub(models.villains, 'findAll')
+                stubbedFindOne = sandbox.stub(models.villains, 'findOne')
 
                 stubbedSend = sandbox.stub()
                 stubbedSendStatus = sandbox.stub()
-                stubbedFindAll = sandbox.stub(models.villains, 'findAll')
+                stubbedStatus = sandbox.stub()
+                stubbedStatusDotSend = sandbox.stub()
 
                 response = {
                     send: stubbedSend,
-                    sendStatus: stubbedSendStatus
-
+                    sendStatus: stubbedSendStatus,
+                    status: stubbedStatus,
                 }
             })
+
+           beforeEach(() => {
+                stubbedStatus.returns({ send: stubbedStatusDotSend })
+            })
+            
 
             afterEach(() => {
                 sandbox.reset()
@@ -51,10 +63,44 @@ const {getAllVillains,
                 await getAllVillains({}, response)
 
                 expect(stubbedFindAll).to.have.callCount(1)
-                expect(stubbedSend).to.have.been.calledWith(mockVillainsList)
+                expect(stubbedStatus).to.have.been.calledWith(500) // doesn't like .calledWith
+                expect(stubbedStatusDotSend).to.have.been.calledWith('Unable to retrieve. Please try again')
             })   
+
+            it('responds with a 500 status and error message when the database throws an error', async() => {
+                stubbedFindAll.throws('Error')
+
+                await getAllVillains({}, response)
+
+                expect(stubbedFindAll).to.have.callCount(1)
+
+            })
+            
         })
         describe('getVillainBySlug', () => {
+            it('Retrieves villain by slug', async() =>{
+                stubbedFindOne = (mockVillain)
+                const request = { params: { slug: 'captain-hook' } }
+
+                await getVillainBySlug({}, response)
+
+                expect (stubbedFindOne).to.have.been.calledWith( { where: { slug: 'captain-hook' } }) // doesn't like .calledWith
+                expect (stubbedSend).to.have.been.calledWith(mockVillain)
+            })
+
+            it('Responds with a 404 status when no match is found', async() =>{
+                stubbedFindOne.return(null)
+                const request = { params: { slug: 'not-found' } }
+
+                await getVillainBySlug({}, response)
+
+                expect (stubbedFindOne).to.have.been.calledWith( { where: { slug: 'not-found' } }) // doesn't like .calledWith
+                expect (stubbedSendStatus).to.have.been.calledWith(404)
+            })
+
+            it('Responds with a 500 status and an error message when the database throws an error', async() =>{
+
+            })
         })
         describe('saveNewVillain  ', () => {
         })
